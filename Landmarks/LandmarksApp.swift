36d5cf235d3a02aacdf9e98b9fb9d255a1c51e5d
Kenji ADACHI <kenji.183927@gmail.com>
2023-05-31 21:41:04 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 安達健二 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
